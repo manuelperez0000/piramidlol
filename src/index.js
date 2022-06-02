53a0482c2d5f4804f6eef9ad80c55ed20cbedf75
manuel perez <manuelperez.0000@gmail.com>
2022-06-01 22:05:03 -0400

    first

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import Web3 from 'web3';
+
+ReactDOM.render(
+      <App />,
+  document.getElementById('root')
+);
+

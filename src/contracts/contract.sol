78f10b3487420027d227fdfbb78ed297169cd7aa
manuel perez <manuelperez.0000@gmail.com>
2022-06-09 22:56:48 -0400

    new contract active

--- src/contracts/contract.sol
+++ src/contracts/contract.sol
@@ -7,33 +7,22 @@ interface Interface {
 
 contract Piramid is Interface {
 
-    uint internal multiplier = 14;
+    uint internal multiplier = 16;
     address payable public contractOwner;
+    address payable public comision;
 
     constructor () payable {
         contractOwner = payable(msg.sender);
+        comision = payable(msg.sender);
     }
 
-    uint public coversId1 = 0;
+    uint public coversId1  = 0;
+    uint public investorId1 = 0;
     bool prev1 = false;
-    address payable public investor1;
     address payable public nextToCollect1;
+    address payable public nextToCollect2;
     mapping(uint => address payable) public covers1;
     mapping(address => uint) public getId1;
-
-    uint public coversId2 = 0;
-    bool prev2 = false;
-    address payable public investor2;
-    address payable public nextToCollect2;
-    mapping(uint => address payable) public covers2;
-    mapping(address => uint) public getId2;
-
-    uint public coversId3 = 0;
-    bool prev3 = false;
-    address payable public investor3;
-    address payable public nextToCollect3;
-    mapping(uint => address payable) public covers3;
-    mapping(address => uint) public getId3;
 
     mapping(address => uint8) public permisions;
 
@@ -41,35 +30,39 @@ contract Piramid is Interface {
     uint  reguard1 = 340*10**multiplier;
     uint  comision1 = price1*2-reguard1;
 
-    uint  price2 =   340*10**multiplier;
-    uint  reguard2 = 612*10**multiplier;
-    uint  comision2 = price2*2-reguard2;
-
-    uint  price3 =   612*10**multiplier;
-    uint  reguard3 = 116*10**multiplier;
-    uint  comision3 = price3*2-reguard3;
-
     function pool1() public payable returns(bool){
         require(msg.value == price1,"Invalid amount to deposit");
         if(prev1 == false && coversId1 == 0){
-            coversId1 = 1;
-            covers1[1] = payable(msg.sender);
-            getId1[msg.sender] = coversId1+1;
+            comision.transfer(comision1);
+            coversId1 = coversId1 + 1;
+            investorId1 = investorId1 +1;
+            covers1[investorId1] = payable(msg.sender);
+            getId1[msg.sender] = investorId1;
             nextToCollect1 = payable(msg.sender);
         }else {
             if(!prev1){
-                investor1 = payable(msg.sender);
                 prev1 = true;
-                getId1[msg.sender] = coversId1+1;
+                investorId1 = investorId1 +1;
+
+                covers1[investorId1] = payable(msg.sender);
+                getId1[msg.sender] = investorId1;
+
+                nextToCollect1 = covers1[coversId1];
+                nextToCollect2 = covers1[coversId1+1];
             }else{
-                covers1[coversId1].transfer(reguard1); 
-                nextToCollect1 = investor1;
-                investor1 = payable(msg.sender);
+                covers1[coversId1].transfer(reguard1);
+                comision.transfer(comision1);
+                
+                investorId1 = investorId1 +1;
+                coversId1 = coversId1+1;
                 prev1 = false;
-                covers1[coversId1+1] = payable(investor1);
-                covers1[coversId1+2] = payable(msg.sender);
-                coversId1 = coversId1+2;
-                getId1[msg.sender] = coversId1+1;
+
+                covers1[investorId1] = payable(msg.sender);
+                getId1[msg.sender] = investorId1;
+                
+                nextToCollect1 = covers1[coversId1];
+                nextToCollect2 = covers1[coversId1+1];
+
             }
         }
         if(permisions[msg.sender] == 0 ) permisions[msg.sender] = 1;
@@ -78,78 +71,140 @@ contract Piramid is Interface {
         return true;
     }
 
-    function pool2() public payable returns (bool){
-        require(permisions[msg.sender] > 0,"You don heve permisions");
-        require(msg.value == price2,"Invalid amount to deposit");
-        if(prev2 == false && coversId2 == 0){
-            coversId2 = 1;
-            covers2[1] = payable(msg.sender);
-            nextToCollect2 = payable(msg.sender);
-            getId2[msg.sender] = coversId2+1;
-        }else {
+    function getNnextToCollect1() public view returns (address){
+        return nextToCollect1;
+    }
+    function getNnextToCollect2() public view returns (address){
+        return nextToCollect2;
+    }
+//**** stake 1
+//******************************************************************************
 
+    uint public coversId2  = 0;
+    uint public investorId2 = 0;
+    bool prev2 = false;
+    address payable public nextToCollect1b;
+    address payable public nextToCollect2b;
+    mapping(uint => address payable) public covers2;
+    mapping(address => uint) public getId2;
+
+    uint  price2 =   340*10**multiplier;
+    uint  reguard2 = 612*10**multiplier;
+    uint  comision2 = price2*2-reguard2;
+
+    function pool2() public payable returns(bool){
+        require(msg.value == price2,"Invalid amount to deposit");
+        require(permisions[msg.sender] > 0);
+        if(prev2 == false && coversId2 == 0){
+            comision.transfer(comision2);
+            coversId2 = coversId2 + 1;
+            investorId2 = investorId2 +1;
+            covers2[investorId2] = payable(msg.sender);
+            getId2[msg.sender] = investorId2;
+            nextToCollect1b = payable(msg.sender);
+        }else {
             if(!prev2){
-                investor2 = payable(msg.sender);
                 prev2 = true;
-                getId2[msg.sender] = coversId2+1;
+                investorId2 = investorId2 +1;
+
+                covers2[investorId2] = payable(msg.sender);
+                getId2[msg.sender] = investorId2;
+
+                nextToCollect1b = covers2[coversId2];
+                nextToCollect2b = covers2[coversId2+1];
             }else{
-                covers2[coversId2].transfer(reguard2); 
-                nextToCollect2 = investor2;
-                investor2 = payable(msg.sender);
+                covers2[coversId2].transfer(reguard2);
+                comision.transfer(comision2);
+                
+                investorId2 = investorId2 +1;
+                coversId2 = coversId2+1;
                 prev2 = false;
-                covers2[coversId2+1] = investor2;
-                covers2[coversId2+2] = payable(msg.sender);
-                coversId2 = coversId2+2;
-                getId2[msg.sender] = coversId2+1;
+
+                covers2[investorId2] = payable(msg.sender);
+                getId2[msg.sender] = investorId2;
+                
+                nextToCollect1b = covers2[coversId2];
+                nextToCollect2b = covers2[coversId2+1];
+
             }
         }
-        permisions[msg.sender] = permisions[msg.sender] + 1;
+        permisions[msg.sender] = permisions[msg.sender] +1;
         uint id = coversId2;
         emit deposit(id);
         return true;
     }
 
-    function pool3() public payable returns (bool){
-        require(permisions[msg.sender] > 1,"You don heve permisions");
-        require(msg.value == price3,"Invalid amount to deposit");
-        if(prev3 == false && coversId3 == 0){
-            coversId3 = 1;
-            covers3[1] = payable(msg.sender);
-            nextToCollect3 = payable(msg.sender);
-            getId3[msg.sender] = coversId3+1;
-        }else {
+    function getNnextToCollect1b() public view returns (address){
+        return nextToCollect1b;
+    }
+    function getNnextToCollect2b() public view returns (address){
+        return nextToCollect2b;
+    }
+//*** stake 2
+//******************************************************************************
+    uint public coversId3  = 0;
+    uint public investorId3 = 0;
+    bool prev3 = false;
+    address payable public nextToCollect1c;
+    address payable public nextToCollect2c;
+    mapping(uint => address payable) public covers3;
+    mapping(address => uint) public getId3;
 
+    uint  price3 =    612*10**multiplier;
+    uint  reguard3 = 1162*10**multiplier;
+    uint  comision3 = price3*2-reguard3;
+
+    function pool3() public payable returns(bool){
+        require(msg.value == price3,"Invalid amount to deposit");
+        require(permisions[msg.sender] > 1,"permisions fail");
+        if(prev3 == false && coversId3 == 0){
+            comision.transfer(comision3);
+            coversId3 = coversId3 + 1;
+            investorId3 = investorId3 +1;
+            covers3[investorId3] = payable(msg.sender);
+            getId3[msg.sender] = investorId3;
+            nextToCollect1c = payable(msg.sender);
+        }else {
             if(!prev3){
-                investor3 = payable(msg.sender);
                 prev3 = true;
-                getId3[msg.sender] = coversId3+1;
+                investorId3 = investorId3 +1;
+
+                covers3[investorId3] = payable(msg.sender);
+                getId3[msg.sender] = investorId3;
+
+                nextToCollect1c = covers3[coversId3];
+                nextToCollect2c = covers3[coversId3+1];
             }else{
-                covers3[coversId3].transfer(reguard3); 
-                nextToCollect3 = investor3;
-                investor3 = payable(msg.sender);
+                covers3[coversId3].transfer(reguard3);
+                comision.transfer(comision3);
+                
+                investorId3 = investorId3 +1;
+                coversId3 = coversId3+1;
                 prev3 = false;
-                covers3[coversId3+1] = investor3;
-                covers3[coversId3+2] = payable(msg.sender);
-                coversId3 = coversId3+2;
-                getId3[msg.sender] = coversId3+1;
+
+                covers3[investorId3] = payable(msg.sender);
+                getId3[msg.sender] = investorId3;
+                
+                nextToCollect1c = covers3[coversId3];
+                nextToCollect2c = covers3[coversId3+1];
+
             }
         }
+        permisions[msg.sender] = permisions[msg.sender] +1;
         uint id = coversId3;
         emit deposit(id);
         return true;
     }
 
-    function getInvestor1() public view returns (address){
-        return investor1;
+    function getNnextToCollect1c() public view returns (address){
+        return nextToCollect1b;
     }
+    function getNnextToCollect2c() public view returns (address){
+        return nextToCollect2b;
+    }
+//*** stake 3
+//******************************************************************************
 
-    function getInvestor2() public view returns (address){
-        return investor2;
-    }
-
-    function getInvestor3() public view returns (address){
-        return investor3;
-    }
 
     modifier onlyOwner() {
         require(msg.sender == contractOwner, "Not owner");
